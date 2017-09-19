847a4038285f892cbd116564292dfb27447d34c2
Angel M De Miguel <angel@bitnami.com>
2017-09-19 10:32:44 +0200

    Add the mongo connection. Fix compose file

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      message: 'Loading...'
+    }
+  }
+
+  componentDidMount() {
+    fetch('/api')
+      .then((response) => {
+        return response.json()
+      }).then((json) => {
+        this.setState({ message: json.message });
+      }).catch((ex) => {
+        console.log('parsing failed', ex);
+      });
+  }
+
   render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    return <h1>{ this.state.message }</h1>;
   }
 }
 
